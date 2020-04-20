8d8d2dc239c29556fd0ef92e9905de8a719bbbef
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-04-21 00:04:28 +0530

    Added ApiResult class that can handle all endpoints result

---
+++ FireAlarmMonitoringSystem - API/FireAlarm.Web.Data/Entities/ApiResult.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireAlarm.Web.Data.Entities
+{
+    public class ApiResult
+    {
+        [JsonProperty("STATUS")]
+        public bool STATUS { get; set; }
+        [JsonProperty("DATA")]
+        public object DATA { get; set; }
+    }
+}
