e65d1226dd2f643182c02df48aa1f498e268b36b
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-04-21 00:06:25 +0530

    Add UserController class to handle requests related to the users

---
+++ FireAlarmMonitoringSystem - API/FireAlarm.Web.API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FireAlarm.Web.Data.Entities;
+using FireAlarm.Web.Data.Persistence;
+
+namespace FireAlarm.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly FireAlarmDbContext _context;
+        private readonly IUserService _userService;
+
+        public UserController(FireAlarmDbContext context, IUserService userService)
+        {
+            _context = context;
+            _userService = userService;
+        }
+    }
+}
