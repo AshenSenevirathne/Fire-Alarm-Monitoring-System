bb20781206ef3c4231788f020b42d708457386bf
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-04-23 23:28:12 +0530

    remove sensor_state table

--- FireAlarmMonitoringSystem - API/FireAlarm.Web.Data/Entities/FireAlarmDbContext.cs
+++ FireAlarmMonitoringSystem - API/FireAlarm.Web.Data/Entities/FireAlarmDbContext.cs
@@ -17,16 +17,10 @@ namespace FireAlarm.Web.Data.Entities
                 .HasOne<UserRole>(u => u.userRole)
                 .WithMany(ur => ur.Users)
                 .HasForeignKey(u => u.userRoleId);
-
-            modelBuilder.Entity<SensorState>()
-                .HasOne<SensorDetails>(ss => ss.sensorDetails)
-                .WithMany(sd => sd.sensorStates)
-                .HasForeignKey(ss => ss.sensorId);
         }
 
         public DbSet<User> Users { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<SensorDetails> SensorDetails { get; set; }
-        public DbSet<SensorState> SensorState { get; set; }
     }
 }
