c65fa8abc5d2814ae64302e190ca694c88798c06
AshenSenevirathne <ashensachin@gmail.com>
2020-05-06 07:16:21 +0530

    Client Constants Added to the System

---
+++ RMI Server And Client/src/com/rmiclient/configurations/ClientConstants.java
@@ -0,0 +1,17 @@
+package com.rmiclient.configurations;
+
+/*
+ * 	@Author 		:	Ashen Senevirathne
+ *	@Class Name		:	ClientConstants
+ *	@Description	:	Used to store the constants value of client programe
+ * 
+*/
+
+public class ClientConstants {
+	
+	// Time interval for update sensor state
+	public static int TIME_PERIOD = 15000;
+	// Time delay
+	public static int TIME_DELAY = 0;
+	
+}
